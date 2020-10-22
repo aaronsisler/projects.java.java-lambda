91dacddcff938544a6847a4101b85e432cbf1f8f
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-10-22 11:28:18 -0400

    Updating for changes to pom and deployment process

--- src/main/java/com/eandbsolutions/HelloHandler.java
+++ src/main/java/com/eandbsolutions/HelloHandler.java
@@ -4,13 +4,21 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import org.joda.time.Instant;
 
+import java.util.Collections;
+import java.util.Map;
 
-public class HelloHandler implements RequestHandler<Object, ApiGatewayProxyResponse> {
-    public ApiGatewayProxyResponse handleRequest(Object input, Context context) {
-        context.getLogger().log(input.toString());
+public class HelloHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        context.getLogger().log(String.format("received: {}", input));
         Instant instant = new Instant();
         String currentTime = instant.toString();
-
-        return new ApiGatewayProxyResponse(200, null, String.format("Hello World! The time is %s", currentTime));
+        Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
+        return ApiGatewayResponse.builder()
+                .setStatusCode(200)
+                .setObjectBody(responseBody)
+                .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+                .build();
     }
 }
