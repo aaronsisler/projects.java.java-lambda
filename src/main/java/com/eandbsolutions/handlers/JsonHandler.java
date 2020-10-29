cac05503276bf206f73713ef42159a052118b8b5
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-10-29 13:48:05 -0400

    Getting the json to be returned correctly from main body key

--- src/main/java/com/eandbsolutions/handlers/JsonHandler.java
+++ src/main/java/com/eandbsolutions/handlers/JsonHandler.java
@@ -4,13 +4,22 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.eandbsolutions.models.ApiGatewayRequest;
 import com.eandbsolutions.models.ApiGatewayResponse;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 public class JsonHandler implements RequestHandler<ApiGatewayRequest, ApiGatewayResponse> {
-    public ApiGatewayResponse handleRequest(ApiGatewayRequest input, Context context) {
-        context.getLogger().log(String.format("received: {}", input));
+    public ApiGatewayResponse handleRequest(ApiGatewayRequest request, Context context) {
+        context.getLogger().log(request.getBody());
 
         try {
-            return new ApiGatewayResponse(200, "Hello there!");
+            JsonObject convertedObject = new Gson().fromJson(request.getBody(), JsonObject.class);
+            JsonElement bodyMain = convertedObject.get("main");
+            context.getLogger().log(bodyMain.toString());
+
+            String responseBody = bodyMain.toString();
+
+            return new ApiGatewayResponse(200, responseBody);
         } catch (Exception e) {
             return new ApiGatewayResponse(500, String.format("Error message: %s", e.getMessage()));
         }
