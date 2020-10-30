55ff8bf3d46f6a7cd91b127841728075bfaf6dbf
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-10-29 21:30:52 -0400

    Tweaks for silliness

--- src/main/java/com/eandbsolutions/App.java
+++ src/main/java/com/eandbsolutions/App.java
@@ -1,8 +1,17 @@
 package com.eandbsolutions;
 
+import com.eandbsolutions.components.DaggerVehiclesComponent;
+import com.eandbsolutions.components.VehiclesComponent;
+import com.eandbsolutions.models.Car;
+
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Here");
+        VehiclesComponent vehiclesComponent = DaggerVehiclesComponent.create();
+
+        Car carOne = vehiclesComponent.buildCar();
+        System.out.println(carOne.getEngine());
+        System.out.println(carOne.getMake());
     }
 }
--- src/main/java/com/eandbsolutions/models/Car.java
+++ src/main/java/com/eandbsolutions/models/Car.java
@@ -3,12 +3,20 @@ package com.eandbsolutions.models;
 import javax.inject.Inject;
 
 public class Car {
-    private Engine engine;
-    private Make make;
+    private final Engine engine;
+    private final Make make;
 
     @Inject
     public Car(Engine engine, Make make) {
-        this.engine = engine;
-        this.make = make;
+        this.engine = engine == null ? new Engine() : engine;
+        this.make = make == null ? new Make() : make;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public Make getMake() {
+        return make;
     }
 }
