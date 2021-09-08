2af3353d64193c8f0553e6077ebc64991125c056
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-09-08 06:47:08 -0400

    Pushing the App.java for sorting

--- src/main/java/com/eandbsolutions/App.java
+++ src/main/java/com/eandbsolutions/App.java
@@ -1,7 +1,33 @@
 package com.eandbsolutions;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class App {
 
     public static void main(String[] args) {
+        int[] unsorted = new int[]{3, 0, 1, 4, 2};
+
+        int j = unsorted.length - 1;
+        int tempLower;
+        for (int i = 1; i < unsorted.length; i++) {
+            if (unsorted[i] < unsorted[i - 1]) {
+                System.out.println(String.format("Moving %d below %d", unsorted[i], unsorted[i - 1]));
+                tempLower = unsorted[i];
+                unsorted[i] = unsorted[i - 1];
+                unsorted[i - 1] = tempLower;
+            }
+
+            if (unsorted[j] < unsorted[j - 1]) {
+                System.out.println(String.format("Moving %d above %d", unsorted[j - 1], unsorted[j]));
+                tempLower = unsorted[j];
+                unsorted[j] = unsorted[j - 1];
+                unsorted[j - 1] = tempLower;
+            }
+        }
+
+        String joined;
+        joined = Arrays.stream(unsorted).mapToObj(String::valueOf).collect(Collectors.joining(","));
+        System.out.println(joined);
     }
 }
--- src/main/java/com/eandbsolutions/handlers/HealthHandler.java
+++ src/main/java/com/eandbsolutions/handlers/HealthHandler.java
@@ -4,14 +4,23 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.eandbsolutions.models.ApiGatewayRequest;
 import com.eandbsolutions.models.ApiGatewayResponse;
-import org.joda.time.Instant;
+
+import java.time.ZonedDateTime;
 
 public class HealthHandler implements RequestHandler<ApiGatewayRequest, ApiGatewayResponse> {
+    private ZonedDateTime startTime;
+
+    public HealthHandler() {
+//        startTime = ZonedDateTime.now();
+    }
+
     public ApiGatewayResponse handleRequest(ApiGatewayRequest input, Context context) {
+        startTime = ZonedDateTime.now();
         context.getLogger().log(String.format("received: {}", input));
-        Instant instant = new Instant();
-        String currentTime = instant.toString();
-        String responseBody = String.format("Hello from Java Lambda. Time is: %s", currentTime);
+//        Instant instant = new Instant();
+//        String currentTime = instant.toString();
+//        String responseBody = String.format("Hello from Java Lambda. Time is: %s", currentTime);
+        String responseBody = String.format("Hello from Java Lambda. Time is: %s", startTime.toString());
 
         return new ApiGatewayResponse(200, responseBody);
     }
